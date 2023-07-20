269690f70feee4d48aa1d32095f8ce5dba13c39d
waltonklavierspieler <lottwa@gmail.com>
2023-07-20 14:27:10 -0600

    Fully functional: can add and remove books successfully. Some styling may still be desired to make the color scheme more synergistic. Need to fix the scaling between new card entries and displayUpdate entries.
    Other ideas: keep track of most common genre?

--- Bookshelf.js
+++ Bookshelf.js
@@ -12,71 +12,79 @@ function Book(title, author, pages, genre) {
 function displayBook() {
 	var counter = 0;
 	const parentDiv = document.querySelector('.list');
+
 	//create new div for each entry
-	
-	parentDiv.innerHTML = '';
+		parentDiv.innerHTML = '';
 	library.forEach((newEntry, index) => {
 		counter++;
 		const book = document.createElement('div');
+		book.setAttribute('class', 'newCard');
 		book.classList.add('displayEntry');
-	
+
 		//create a div for each element of each book for styling
 		const bookTitle = document.createElement('div');
 		bookTitle.className = 'bookTitles';
 		bookTitle.textContent = newEntry.title;
-		
+
+		const bookBy = document.createElement('p');
+		bookBy.className = 'bookBy';
+		bookBy.textContent = 'by';
+
 		const bookAuthor = document.createElement('div');
 		bookAuthor.className = 'bookAuthors';
 		bookAuthor.textContent = newEntry.author;
-		
+
 		const bookPages = document.createElement('div');
 		bookPages.className = 'bookPages';
-		bookPages.textContent = newEntry.pages;
-		
+		bookPages.textContent = newEntry.pages + ' pages';
+
 		const bookGenre = document.createElement('div');
 		bookGenre.className = 'bookGenre';
 		bookGenre.textContent = newEntry.genre;
-		
+
 		//add a new value to the book to access its index;
 		newEntry.removalIndex = index;
-		
+
 		const remove = document.createElement('button');
+		remove.setAttribute('id', 'removeBookButton');
 		remove.addEventListener('click', deleteBook);
 		remove.textContent = 'Remove entry';
-		
+
 		//append the sub divs
 		book.appendChild(bookTitle);
-		book.appendChild(bookAuthor);
+		bookBy.appendChild(bookAuthor);
+		book.appendChild(bookBy);
+		//book.appendChild(bookAuthor);
 		book.appendChild(bookPages);
 		book.appendChild(bookGenre);
 		book.appendChild(remove);
-		
+
 		//append the whole entry to the mainline
 		parentDiv.appendChild(book);
 	});
-	
+
 	const bookTally = document.getElementById('books-total');
 	bookTally.innerHTML = 'Books in your library: ' + counter;
 }
 
 //create click events for the buttons.
-const bookButton = document.getElementById('book-button');
+const bookButton = document.getElementById('addBookButton');
 bookButton.addEventListener('click', addBook);
 
 function addBook() {
 	const newForm = document.createElement("form");
+	newForm.setAttribute('class', 'newCard');
 	newForm.innerHTML = `
 		<label for="title">Title:</label>
 		<input type="text" id="title" required><br>
-		
+
 		<label for="author">Author:</label>
 		<input type="text" id="author" required><br>
-		
+
 		<label for="pages">Pages:</label>
 		<input type="number" id="pages" required><br>
-		
+
 		<fieldset>
-			<legend>Genre:</legend>
 			<input type="radio" id="sci-fi" name="genre" value="Sci-fi">
 			<label for="sci-fi">Sci-Fi</label><br>
 			<input type="radio" id="fiction" name="genre" value="Fiction">
@@ -88,20 +96,20 @@ function addBook() {
 			<input type="radio" id="non-fiction" name="genre" value="Non-fiction">
 			<label for="non-fiction">Non-fiction</label><br>
 		</fieldset>
-		
-		<button type="submit">Add book</button>
+
+		<button id="addBookButton" type="submit">+ Add book</button>
 	`;
-	
+
 	newForm.onsubmit = (event) =>  {
 		event.preventDefault();
-		
+
 		const title = newForm.querySelector("#title").value;
 		const author = newForm.querySelector("#author").value;
 		const pages = newForm.querySelector("#pages").value;
 		const genre = newForm.querySelector('input[name=genre]:checked').value;
 		const newBook = new Book(title, author, pages, genre);
         library.push(newBook);
-		
+
 		displayBook();
 		newForm.reset()
 	}
@@ -115,4 +123,3 @@ function deleteBook(index) {
 }
 
 
-	
